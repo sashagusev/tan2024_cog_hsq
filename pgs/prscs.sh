dacfc3cd3d14952db5b7984aad12b1a3425088f6
Sasha Gusev <alexander_gusev@dfci.harvard.edu>
2025-03-26 21:54:20 -0400

    Update prscs.sh

--- pgs/prscs.sh
+++ pgs/prscs.sh
@@ -2,7 +2,7 @@
 
 GWAS=$1
 CHR=$2
-GWAS_N=`cat $GWAS.sumstats | tail -n+2 | awk '{ print $NF }' | awk -f ~/tools/avg.awk | awk '{ print int($1) }'`
+GWAS_N=$3
 
 PRScs.py \
 --ref_dir=ldblk_ukbb_eur \
@@ -10,4 +10,22 @@ PRScs.py \
 --sst_file=$GWAS.sumstats \
 --n_gwas=${GWAS_N} \
 --out_dir=$GWAS.prscs \
+--chrom=$CHR
+
+PRScs.py \
+--ref_dir=ldblk_ukbb_eur \
+--bim_prefix=phase3_maf01 \
+--sst_file=$GWAS.sumstats \
+--n_gwas=${GWAS_N} \
+--out_dir=$GWAS.prscs.phi_e2. \
+--phi=1e-2 \
+--chrom=$CHR
+
+PRScs.py \
+--ref_dir=ldblk_ukbb_eur \
+--bim_prefix=phase3_maf01 \
+--sst_file=$GWAS.sumstats \
+--n_gwas=${GWAS_N} \
+--out_dir=$GWAS.prscs.phi_e4. \
+--phi=1e-4 \
 --chrom=$CHR
